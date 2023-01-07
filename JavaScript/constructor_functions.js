f0bd942f7e4b3943d5cd0b9d6f3a89732f183969
yashy2710@gmail.com <yashy2710@gmail.com>
2023-01-07 13:35:06 +0530

    um

--- JavaScript/constructor_functions.js
+++ JavaScript/constructor_functions.js
@@ -1,39 +1,36 @@
-//Ways to create objects
+
+
+//ways to create objects
 let animal = {};
-animal.name = "tiger"
-animal.sound = "roar"
+animal.name = "Tiger";
+animal.sound = "roar";
 
 //second way
 let animal1 = {
-    name: "Tiger",
-    sound: "roar",
+    name:"Tiger",
+    sound:"roar",
     eat:function(){
-        
     }
 }
 
 let animal2 = {
-    name: "Pig",
-    sound: "Grunt",
+    name:"Pig",
+    sound:'Grunt',
     sleep:function(){
-        
     }
 }
 animal2.name;
 
-function Animal(name1, sound1){
-    this.name = name1
-    this.sound = sound1
-
-    
+function Animal(name, sound1){    
+    this.name = name;
+    this.sound = sound1;   
 }
 
 let Tiger = new Animal("Tiger", "Roar");
-let pig = new Animal("Pig", "Grunt");
+let pig = new Animal("Pig","Grunt");
 
 console.log(Tiger.name);
 console.log(pig.name);
-
 
 
 
