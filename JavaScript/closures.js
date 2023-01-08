69668a1afcb47075f7534401cc70bcc968c6a545
yashy2710@gmail.com <yashy2710@gmail.com>
2023-01-08 20:03:35 +0530

    closures

--- JavaScript/closures.js
+++ JavaScript/closures.js
@@ -12,3 +12,24 @@ function greeting(){
 
 let hi = greeting();
 hi();
+
+
+// let counter = 0;
+function add(counter){
+    let counter = 0;
+    
+    return function(){
+        counter += 1
+        return counter;
+    }
+}
+
+console.log(add());
+console.log(add());
+
+let x1 = add(0);
+console.log(x1)
+let x2 = add(x1);
+console.log(x2)
+
+// console.log(add());
