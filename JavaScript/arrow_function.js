a9cb032a9bbdac45f82e7c486e6ada1d092e7689
Yash <98265000+yash-yadav-kumar@users.noreply.github.com>
2023-01-15 21:10:50 +0530

    commit

---
+++ JavaScript/arrow_function.js
@@ -0,0 +1,17 @@
+let addTwoNum = (num1, num2) => num1 + num2;
+
+var sqr = num =>{
+    var res = num + num;
+    return res*res
+}
+
+var arr = [5, 6, 3, 1, 4]
+arr.sort();
+
+arr.forEach
+
+console.log(arr);
+
+console.log(addTwoNum(1, 2));
+console.log(sqr(10));
+console.log(arr);
