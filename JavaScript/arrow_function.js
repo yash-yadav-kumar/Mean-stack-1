0e1f83cfc1e2f12070670a712cb7087ae453a6c1
Yash <98265000+yash-yadav-kumar@users.noreply.github.com>
2023-01-15 21:21:07 +0530

    Next commit

--- JavaScript/arrow_function.js
+++ JavaScript/arrow_function.js
@@ -1,17 +1,24 @@
 let addTwoNum = (num1, num2) => num1 + num2;
 
+console.log("Default value: ", addTwoNum(1));
 var sqr = num =>{
     var res = num + num;
     return res*res
 }
 
 var arr = [5, 6, 3, 1, 4]
-arr.sort();
+// arr.sort();
 
-arr.forEach
+arr.forEach(function(res){
+    console.log(res);
+});
+
+setTimeout(()=>{
+    
+}, 1000);
 
 console.log(arr);
 
-console.log(addTwoNum(1, 2));
-console.log(sqr(10));
-console.log(arr);
+// console.log(addTwoNum(1, 2));
+// console.log(sqr(5));
+// console.log(arr);
